d28f2273ac01022eeb5cd11f65f776625f29b9a8
amasdeogratias <deogratiasprog@gmail.com>
2022-11-29 14:37:08 +0300

    implementing search in location lists api

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 # define paths
 urlpatterns = [
-    path('', views.endpoints )
+    path('', views.endpoints ),
+    path('locations/', views.location_list),
+    path('locations/<str:name>/', views.location_detail)
 ]
--- api/views.py
+++ api/views.py
@@ -4,10 +4,29 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Location
 from .serializers import LocationSerializer
+from django.db.models import Q
 
 # Create your views here.
 @api_view(['GET'])
 def endpoints(request):
-    locations = Location.objects.all()
+    data = ['/locations','locations/:name']
+    return Response(data)
+ 
+@api_view(['GET'])    
+def location_list(request):
+    # /locations/?query=Pune
+    query = request.GET.get('query')
+    
+    if query == None:
+        query=''
+    #single and multiple search
+    locations = Location.objects.filter(Q(name__icontains=query)| Q(latitude__icontains=query))
     serializer = LocationSerializer(locations, many=True)
+    return Response(serializer.data)
+    
+# return single location by name
+@api_view(['GET'])
+def location_detail(request,name):
+    locations = Location.objects.get(name=name)
+    serializer = LocationSerializer(locations, many=False)
     return Response(serializer.data)
