c774547a4b9884214aa527a8ed9adf6cd37566d1
amasdeogratias <deogratiasprog@gmail.com>
2022-11-30 16:13:57 +0300

    added post request for location api

--- api/views.py
+++ api/views.py
@@ -12,17 +12,32 @@ def endpoints(request):
     data = ['/locations','locations/:name']
     return Response(data)
  
-@api_view(['GET'])    
+@api_view(['GET', 'POST'])    
 def location_list(request):
-    # /locations/?query=Pune
-    query = request.GET.get('query')
-    
-    if query == None:
-        query=''
-    #single and multiple search
-    locations = Location.objects.filter(Q(name__icontains=query)| Q(latitude__icontains=query))
-    serializer = LocationSerializer(locations, many=True)
-    return Response(serializer.data)
+    # Handle GET request
+    if request.method=='GET':
+        
+        # /search option in list
+        # /locations/?query=Pune
+        query = request.GET.get('query')
+        
+        if query == None:
+            query=''
+        #single and multiple search
+        locations = Location.objects.filter(Q(name__icontains=query)| Q(latitude__icontains=query))
+        serializer = LocationSerializer(locations, many=True)
+        return Response(serializer.data)
+        
+    # Handle POST request
+    if request.method == 'POST':
+        location = Location.objects.create(
+            name = request.data['name'],
+            latitude=request.data['latitude'],
+            longitude=request.data['longitude']
+        )
+        serializer = LocationSerializer(location, many=False)
+        return Response(serializer.data)
+        
     
 # return single location by name
 @api_view(['GET'])
